aa4cf8b24cb01f4ed58803f50b00b64a54d50f64
Roberts Ozoliņš <47858537+ohbob@users.noreply.github.com>
2021-01-15 23:32:54 +0200

    d
    d

---
+++ .routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "svelte",
+    "html",
+    "svx",
+    "md"
+  ],
+  "started": "2021-01-15T21:28:34.414Z"
+}
---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	const wordpress_url = "vietnieks.com"
+
+	async function getData() {
+		const response = await fetch("http://" + wordpress_url + "/wp-json/wp/v2/posts");
+
+		if (response.ok) {
+			return response.json();
+
+		} else {
+			throw new Error('there was a problem with fetch');
+		}
+	}
+</script>
+
+{#await getData()}
+	loading...
+{:then posts}
+	{#each posts as post}
+		<section>
+			<h2>{post.title.rendered}</h2>
+			<p>{@html post.content.rendered}</p>
+		</section>
+	{/each}
+
+{:catch e}
+	error {e}
+{/await}
+
+<style>
+	section{
+		margin: 0 auto;
+		max-width:720px;
+	}
+</style>
